4e28762e96627a0d719dde2b746627efdf865f7a
Haydn1258 <51240639+Haydn1258@users.noreply.github.com>
2019-08-06 18:24:50 +0900

    "흐엉"

---
+++ app/src/main/java/com/example/mysns/Comment.kt
@@ -0,0 +1,10 @@
+package com.example.mysns
+
+class Comment{
+    var commnetId =""
+    var postId = ""
+    var writerId = ""
+    var message = ""
+    var writeTime:Any = Any()
+    var bgUri=""
+}
---
+++ app/src/main/java/com/example/mysns/Post.kt
@@ -0,0 +1,10 @@
+package com.example.mysns
+
+class Post{
+    var postID =""
+    var writerId = ""
+    var message = ""
+    var writeTime:Any = Any()
+    var bgUri = ""
+    var commentCount = 0
+}
